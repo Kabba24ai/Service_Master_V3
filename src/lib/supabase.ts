2527a213647272826a6f516dfae8055a0f618719
Qin <74532323+qinxiaodong97@users.noreply.github.com>
2025-11-26 10:25:36 -0500

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://0ec90b57d6e95fcbda19832f.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJib2x0IiwicmVmIjoiMGVjOTBiNTdkNmU5NWZjYmRhMTk4MzJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2MjQyMDAsImV4cCI6MjA0ODIwMDIwMH0.QGHR3jLlqYfCWWZ5OODqBOLKD2R8xLx8V8pZQGJZ0lU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://kfyecqjbevkokuirpdel.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmeWVjcWpiZXZrb2t1aXJwZGVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNDI1NzksImV4cCI6MjA3OTcxODU3OX0.Xw1vXS9qhLw126g44oBK3vTfW_ga4QDg_GKp_Tz5ZuY';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
